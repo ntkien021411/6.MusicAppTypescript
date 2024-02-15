d770d9df7a5a07fcf4250e157de5fb2ec029c206
K1EL2002 <94553932+ntkien021411@users.noreply.github.com>
2024-02-15 18:47:13 +0700

    Install pug and create views

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("ok");
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
+const port: Number | String = 3000;
+
+
+// dùng ngôn ngữ pug
+app.set("views", `./views`);
+app.set("view engine", "pug");
+
+//Nhúng phải tĩnh css,js,images
+app.use(express.static(`./public`));
+
+app.get("/topics", async (req:Request , res:Response) =>{
+    res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
