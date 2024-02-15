1a6e130879c74dec2a177efc17d8d3087de15c2f
K1EL2002 <94553932+ntkien021411@users.noreply.github.com>
2024-02-15 21:33:10 +0700

    Làm trang Chủ đề bài hát (bên client)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -6,9 +6,9 @@ export const topics = async (req: Request, res: Response) => {
   const topics = await Topic.find({
     deleted: false,
   });
-  console.log(topics);
 
   res.render("client/pages/topics/index",{
-    title : "Chủ đề bài hát"
+    title : "Chủ đề bài hát",
+    topics:topics
   });
 };
